50dc661b42f3aeb9978ecb4f5e7ac51794efad3a
Ozal Khan <ozalkhan196@gmail.com>
2019-08-13 14:27:22 -0400

    first commit

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+# classbased views
+urlpatterns = [
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path("users/", views.UserList.as_view())
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+]
+
+# function based views
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>', views.snippet_detail),
+# ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
